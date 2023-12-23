ba12b3abec7424b9d3d9ac1b24a19ae1b558cccd
Misha <73448452+Shaytan-M@users.noreply.github.com>
2023-12-23 20:33:48 +0200

    Commit

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { fileURLToPath, URL } from 'node:url';
 
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import path from 'path';
+import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,9 +11,12 @@ export default defineConfig({
         include: ['@fawmi/vue-google-maps', 'fast-deep-equal'],
     },
     resolve: {
-        alias: {
-            '@': path.resolve(__dirname, './src'),
-        },
+        alias: [
+            {
+                find: '@',
+                replacement: path.resolve(__dirname, 'src'),
+            },
+        ],
     },
     css: {
         preprocessorOptions: {
